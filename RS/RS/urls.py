c3fe0352c6964295e75d3b50cb789ea2f3f3d13f
sigmund365 <sigmund365@163.com>
2024-05-19 22:57:10 +0800

    Full Version

--- RS/RS/urls.py
+++ RS/RS/urls.py
@@ -19,5 +19,41 @@ from drugs_rec import views
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('query_user_info', views.query_user_info, name='query_user_info'),
+    
+    #----------------医生功能相关映射---------------
+    path('doctor_menu/<str:user_id>/', views.doctor_menu, name='doctor_menu'),
+    #患者信息管理
+    path('patient_info_management/', views.patient_info_management, name='patient_info_management'),
+    path('add_patient_info/', views.add_patient_info, name='add_patient_info'),
+    path('query_patient_info/', views.query_patient_info, name='query_patient_info'),
+
+    #药物信息管理
+    path('drug_info_management/', views.drug_info_management, name='drug_info_management'),
+    path('add_drug_info/', views.add_drug_info, name='add_drug_info'),
+    path('query_drug_info/', views.query_drug_info, name='query_drug_info'),
+
+    #系统用户管理
+    path('system_user_management/', views.system_user_management, name='system_user_management'),
+    path('add_user_info/', views.add_user_info, name='add_user_info'),
+    path('query_sysuser_info/', views.query_sysuser_info, name='query_sysuser_info'),
+
+    #----------------患者功能相关映射---------------
+    path('patient_menu/<str:user_id>/', views.patient_menu, name='patient_menu'),
+    #抑郁程度评估
+    path('depression_assessment/<str:user_id>/', views.depression_assessment, name='depression_assessment'),
+    path('madrs_assessment_submit/<str:user_id>/', views.madrs_assessment_submit, name='madrs_assessment_submit'),
+    
+    #焦虑程度评估
+    path('anxiety_assessment/<str:user_id>/', views.anxiety_assessment, name='anxiety_assessment'),
+    path('hars_assessment_submit/<str:user_id>/', views.hars_assessment_submit_view, name='hars_assessment_submit'),
+    
+    #个人信息管理
+    path('personal_info_management/<str:user_id>/', views.personal_info_management, name='personal_info_management'),
+    
+    #获取药物诊疗推荐信息
+    path('get_medication_advice/<str:user_id>/', views.get_medication_advice, name='get_medication_advice'),
     path('drug_list', views.drug_list, name='drug_list'),
+
+
+
 ]
--- RS/drugs_rec/views.py
+++ RS/drugs_rec/views.py
@@ -1,10 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import SumAggregator
 import csv  # Assuming CSV format for data files
-from tensorflow.keras.models import load_model
-import tensorflow as tf
-
 
 def welcome(request):
     return render(request, 'welcome.html')
@@ -12,22 +8,144 @@ def welcome(request):
 def query_user_info(request):
     if request.method == 'POST':
         user_id = request.POST.get('user_id')
-        try:
-            user_data = read_user_data()
-            user = find_user_by_id(user_data, user_id)
-            if user:
-                return render(request, 'confirm_user_info.html', {'user': user})
-            else:
-                return HttpResponse('User ID not found.')
-        except Exception as e:
-            return HttpResponse('Error: {}'.format(str(e)))
+        if user_id.startswith('P'):
+            # 如果用户ID以'P'开头，返回患者菜单界面
+            user_id=user_id.replace('P','')
+            return redirect('patient_menu', user_id=user_id)
+        elif user_id.startswith('D'):
+            # 如果用户ID以'D'开头，跳转到医生菜单界面
+            return redirect('doctor_menu', user_id=user_id)
+        else:
+            return HttpResponse('Invalid input format. Please enter a valid user ID.')
     else:
         return HttpResponse('Invalid request.')
+#------------------------------------医生相关的功能---------------------------------------
+def doctor_menu(request, user_id):
+    # 处理医生菜单页面的逻辑
+    return render(request, 'doctor_menu.html', {'user_id': user_id})
+
+#-----------------患者信息管理-------------------
+def patient_info_management(request):
+    return render(request, 'patient_info_management.html')
+
+def add_patient_info(request):
+    if request.method == 'POST':
+        add_name = request.POST.get('add_name')
+        add_gender = request.POST.get('add_gender')
+        add_age = request.POST.get('add_age')
+        add_email = request.POST.get('add_email')
+        # 进行添加患者信息的逻辑处理
+        return HttpResponse('Patient Information Added Successfully.')
+    else:
+        return HttpResponse('Invalid request.')
+
+def query_patient_info(request):
+    if request.method == 'POST':
+        query_patient_id = request.POST.get('query_patient_id')
+        # 进行查询患者信息的逻辑处理
+        return HttpResponse('Query Result for Patient ID {}: ...'.format(query_patient_id))
+    else:
+        return HttpResponse('Invalid request.')
+    
+#-----------------药物信息管理-------------------
+def drug_info_management(request):
+    # 处理药物信息管理逻辑
+    return render(request, 'drug_info_management.html')
+
+def add_drug_info(request):
+    if request.method == 'POST':
+        add_drug_id = request.POST.get('add_drug_id')
+        add_drug_name = request.POST.get('add_drug_name')
+        add_drug_type = request.POST.get('add_drug_type')
+        # 进行新增药物信息的逻辑处理
+        return HttpResponse('Drug Information Added Successfully.')
+    else:
+        return HttpResponse('Invalid request.')
+    
+def query_drug_info(request):
+    if request.method == 'POST':
+        query_drug_id = request.POST.get('query_drug_id')
+        # 进行查询药物信息的逻辑处理
+        return HttpResponse('Query Result for Drug ID {}: ...'.format(query_drug_id))
+    else:
+        return HttpResponse('Invalid request.')
+
+#-----------------系统用户管理-------------------
+def system_user_management(request):
+    return render(request, 'system_user_management.html')
+
+def add_user_info(request):
+    if request.method == 'POST':
+        add_user_id = request.POST.get('add_user_id')
+        add_user_password = request.POST.get('add_user_password')
+        add_user_role = request.POST.get('add_user_role')
+        # 进行新增用户信息的逻辑处理，包括密码
+        return HttpResponse('User Information Added Successfully.')
+    else:
+        return HttpResponse('Invalid request.')
+
+def query_sysuser_info(request):
+    if request.method == 'POST':
+        query_user_id = request.POST.get('query_sysuser_id')
+        # 进行查询用户信息的逻辑处理
+        return HttpResponse('Query Result for User ID {}: ...'.format(query_user_id))
+    else:
+        return HttpResponse('Invalid request.')
+
+#------------------------------------患者相关的功能---------------------------------------
+def patient_menu(request,user_id):
+    return render(request, 'patient_menu.html', {'user_id': user_id})
+
+def depression_assessment(request,user_id):
+    return render(request, 'depression_assessment.html', {'user_id': user_id})
+
+def madrs_assessment_submit(request,user_id):
+    if request.method == 'POST':
+        # 处理提交的表单数据
+        question1 = request.POST.get('question1')
+        # 处理更多问题的数据
+
+        # 在这里处理评估逻辑，例如计算得分等
+        # 返回评估结果或者跳转到结果页面
+    else:
+        return HttpResponse('Invalid request.')  # 处理非 POST 请求的情况
+
+#焦虑程度评估
+def anxiety_assessment(request,user_id):
+    return render(request, 'anxiety_assessment.html', {'user_id': user_id})
+
+def hars_assessment_submit_view(request,user_id):
+    if request.method == 'POST':
+        # 处理表单提交逻辑
+        question1 = request.POST.get('question1')
+        question2 = request.POST.get('question2')
+        question3 = request.POST.get('question3')  
+        question4 = request.POST.get('question4')
+        
+        # 这里可以根据表单提交的内容进行相应的处理，比如计算得分等
+        
+        return HttpResponse('Assessment submitted successfully.')
+    else:
+        return HttpResponse('Invalid request method.')
+
+#个人信息管理
+def personal_info_management(request,user_id):
+    user_data = read_user_data()
+    user = find_user_by_id(user_data, user_id)
+    if user:
+        return render(request, 'confirm_user_info.html', {'user': user})
+    else:
+        return HttpResponse('User ID not found.')
+    
+
+#获取药物诊疗推荐信息
+def get_medication_advice(request,user_id):
+    return render(request, 'get_medication_advice.html', {'user_id': user_id})
 
 def drug_list(request):
     if request.method == 'POST':
         user_id = request.POST.get('user_id')
-        k=int(request.POST.get('k'))
+        k=int(request.POST.get('k_value'))
         try:
             # Placeholder for recommendation module
             recommended_drugs = recommend_drugs(user_id,k)
@@ -38,6 +156,8 @@ def drug_list(request):
             return HttpResponse('Error: {}'.format(str(e)))
     else:
         return HttpResponse('Invalid request.')
+
+#------------------------------------数据处理---------------------------------------
 
 def read_user_data():
     # Code to read user data from file
